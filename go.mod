d6b33c6bc0bbfa594155383e63ded1eeefcf05ac
干净 <54979375+du5@users.noreply.github.com>
2020-12-13 14:38:56 +0800

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/du5/ups-poweroff
+
+go 1.15
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663
+	github.com/go-redis/redis/v8 v8.4.2
+	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
+	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
+)
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+
+	"context"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/go-ping/ping"
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+type CPing struct {
+	Host    string `toml:"host"`
+	Retry   int    `toml:"retry"`
+	Timeout string `toml:"timeout"`
+}
+
+type Config struct {
+	Hostname string `toml:"hostname"`
+	System   string `toml:"system"`
+	Redis    string `toml:"redis"`
+	Passwd   string `toml:"passwd"`
+	Port     int    `toml:"port"`
+	Ping     CPing  `toml:"ping"`
+	Max      int    `toml:"max"`
+}
+
+func (c Config) getCommand() (string, []string) {
+	if c.System == "macos" {
+		return "osascript", []string{"-e", `tell app "loginwindow" to «event aevtrsdn»`}
+	} else if c.System == "windows" {
+		return "shutdown", []string{"-s", "-t", "0"}
+	} else if c.System == "linux" {
+		return "poweroff", []string{}
+	} else {
+		return "exit", []string{}
+	}
+}
+func (c Config) setCountPP(rdb *redis.Client, count int) {
+	_ = rdb.Set(ctx, fmt.Sprintf("%s.loss", c.Hostname), strconv.Itoa(count), 0)
+}
+
+func (c Config) getCountPP(rdb *redis.Client) int {
+	val, err := rdb.Get(ctx, fmt.Sprintf("%s.loss", c.Hostname)).Result()
+	if err == redis.Nil {
+		val = "0"
+	}
+	int, _ := strconv.Atoi(val)
+	return int
+}
+
+func main() {
+	var conf Config
+	if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
+		panic(err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", conf.Redis, conf.Port),
+		Password: conf.Passwd,
+		DB:       0,
+	})
+
+	pinger, _ := ping.NewPinger(conf.Ping.Host)
+	pinger.Count = conf.Ping.Retry
+	timeOut, _ := time.ParseDuration(conf.Ping.Timeout)
+	pinger.Timeout = timeOut
+	err := pinger.Run()
+	var stats *ping.Statistics
+	if err == nil {
+		stats = pinger.Statistics()
+	}
+	isLoss := false
+	println(stats.PacketsRecv)
+	println(stats.PacketsSent)
+	if err != nil || stats.PacketsSent > 0 {
+		if err != nil || stats.PacketsRecv == 0 {
+			conf.setCountPP(rdb, conf.getCountPP(rdb)+1)
+			isLoss = true
+		}
+	}
+	if !isLoss {
+		conf.setCountPP(rdb, 0)
+	}
+	if conf.getCountPP(rdb) > conf.Max {
+		stra, strb := conf.getCommand()
+		cmd := exec.Command(stra, strb...)
+		err = cmd.Run()
+	}
+}
